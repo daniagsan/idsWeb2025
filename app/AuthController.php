92b470a27c589296c953c02ee399c9be820c5256
daniagsan <jdas_9920@hotmail.com>
2025-11-12 19:55:33 -0700

    Update

--- app/AuthController.php
+++ app/AuthController.php
@@ -1,19 +1,6 @@
 <?php 
   
-/* echo json_encode( $_SERVER ); */
-/* var_dump($_GET); */
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-if($username == "admin"){
-    if($password == "12345"){
-
-        header('Location: ../home.html');
-    }
-}else{
-    header('Location: ../login.html');
-}
-
+    $servidor
 
 
 ?>
